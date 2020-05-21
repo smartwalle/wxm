b65e78f2c0b6ea7b52798b7002dd6cb34fd7d4bd
smartwalle <smartwalle@gmail.com>
2020-05-21 18:17:37 +0800

    添加批量获取用户信息接口

--- user.go
+++ user.go
@@ -9,6 +9,7 @@ import (
 const (
 	kGetUserOpenIdListURL = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=%s"
 	kGetUserInfoURL       = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s"
+	kGetUserInfoListURL   = "https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token=%s"
 )
 
 // GetUserOpenIdList 公众号-获取帐号的关注者列表 https://developers.weixin.qq.com/doc/offiaccount/User_Management/Getting_a_User_List.html
@@ -33,6 +34,25 @@ func (this *Client) GetUserInfo(openId, lang string) (result *GetUserInfoRsp, er
 	values.Add("lang", lang)
 
 	data, err := this.request(http.MethodGet, kGetUserInfoURL, nil, values)
+	if err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(data, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// GetUserInfoList 公众号-批量获取用户基本信息
+func (this *Client) GetUserInfoList(openIds ...string) (result *GetUserInfoListRsp, err error) {
+	if len(openIds) == 0 {
+		return &GetUserInfoListRsp{}, nil
+	}
+
+	var param = &GetUserInfoListParam{}
+	param.AddOpenId(openIds...)
+
+	data, err := this.request(http.MethodPost, kGetUserInfoListURL, param, nil)
 	if err != nil {
 		return nil, err
 	}
--- user_type.go
+++ user_type.go
@@ -11,24 +11,51 @@ type GetUserOpenIdListRsp struct {
 	NextOpenId string `json:"next_openid"`
 }
 
+type UserInfo struct {
+	Subscribe      int    `json"subscribe"`
+	OpenId         string `json"openid"`
+	Nickname       string `json"nickname"`
+	Sex            int    `json"sex"`
+	Language       string `json"language"`
+	City           string `json"city"`
+	Province       string `json"province"`
+	Country        string `json"country"`
+	HeadImgURL     string `json"headimgurl"`
+	SubscribeTime  int64  `json"subscribe_time"`
+	UnionId        string `json"unionid"`
+	Remark         string `json"remark"`
+	GroupId        int64  `json"groupid"`
+	TagIdList      int64  `json"tagid_list"`
+	SubscribeScene string `json"subscribe_scene"`
+	QRScene        int64  `json"qr_scene"`
+	QRSceneStr     string `json"qr_scene_str"`
+}
+
 type GetUserInfoRsp struct {
-	ErrCode        ErrCode `json:"errcode"`
-	ErrMsg         string  `json:"errmsg"`
-	Subscribe      int     `json"subscribe"`
-	OpenId         string  `json"openid"`
-	Nickname       string  `json"nickname"`
-	Sex            int     `json"sex"`
-	Language       string  `json"language"`
-	City           string  `json"city"`
-	Province       string  `json"province"`
-	Country        string  `json"country"`
-	HeadImgURL     string  `json"headimgurl"`
-	SubscribeTime  int64   `json"subscribe_time"`
-	UnionId        string  `json"unionid"`
-	Remark         string  `json"remark"`
-	GroupId        int64   `json"groupid"`
-	TagIdList      int64   `json"tagid_list"`
-	SubscribeScene string  `json"subscribe_scene"`
-	QRScene        int64   `json"qr_scene"`
-	QRSceneStr     string  `json"qr_scene_str"`
+	ErrCode ErrCode `json:"errcode"`
+	ErrMsg  string  `json:"errmsg"`
+	UserInfo
+}
+
+type GetUserInfoListParam struct {
+	UserList []map[string]string `json:"user_list"`
+}
+
+func (this *GetUserInfoListParam) AddOpenId(openIds ...string) {
+	if len(openIds) == 0 {
+		return
+	}
+	if len(this.UserList) == 0 {
+		this.UserList = make([]map[string]string, 0, len(openIds))
+	}
+
+	for _, openId := range openIds {
+		this.UserList = append(this.UserList, map[string]string{"openid": openId})
+	}
+}
+
+type GetUserInfoListRsp struct {
+	ErrCode      ErrCode     `json:"errcode"`
+	ErrMsg       string      `json:"errmsg"`
+	UserInfoList []*UserInfo `json:"user_info_list"`
 }
