0dc835e75e2c358c0ec52098076a5c6de9106f54
smartwalle <smartwalle@gmail.com>
2021-12-16 09:35:37 +0800

    fmt

---
+++ cmd/go.mod
@@ -0,0 +1,10 @@
+module github.com/smartwalle/wxm/cmd
+
+require (
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/smartwalle/wxm v0.0.0
+)
+
+replace github.com/smartwalle/wxm => ../
+
+go 1.13
--- user_type.go
+++ user_type.go
@@ -43,35 +43,35 @@ type GetUserBaseInfoRsp struct {
 }
 
 type UserBaseInfo struct {
-	OpenId     string   `json:"openid"`
-	Nickname   string   `json:"nickname"`
-	Sex        int      `json:"sex"`
-	City       string   `json:"city"`
-	Province   string   `json:"province"`
-	Country    string   `json:"country"`
+	OpenId   string `json:"openid"`
+	Nickname string `json:"nickname"`
+	//Sex        int      `json:"sex"`
+	//City       string   `json:"city"`
+	//Province   string   `json:"province"`
+	//Country    string   `json:"country"`
 	HeadImgURL string   `json:"headimgurl"`
 	UnionId    string   `json:"unionid"`
 	Privilege  []string `json:"privilege"`
 }
 
 type UserInfo struct {
-	Subscribe      int    `json:"subscribe"`
-	OpenId         string `json:"openid"`
-	Nickname       string `json:"nickname"`
-	Sex            int    `json:"sex"`
-	Language       string `json:"language"`
-	City           string `json:"city"`
-	Province       string `json:"province"`
-	Country        string `json:"country"`
-	HeadImgURL     string `json:"headimgurl"`
-	SubscribeTime  int64  `json:"subscribe_time"`
-	UnionId        string `json:"unionid"`
-	Remark         string `json:"remark"`
-	GroupId        int64  `json:"groupid"`
-	TagIdList      int64  `json:"tagid_list"`
-	SubscribeScene string `json:"subscribe_scene"`
-	QRScene        int64  `json:"qr_scene"`
-	QRSceneStr     string `json:"qr_scene_str"`
+	Subscribe int    `json:"subscribe"`
+	OpenId    string `json:"openid"`
+	Nickname  string `json:"nickname"`
+	//Sex            int    `json:"sex"`
+	Language string `json:"language"`
+	//City           string `json:"city"`
+	//Province       string `json:"province"`
+	//Country        string `json:"country"`
+	HeadImgURL     string  `json:"headimgurl"`
+	SubscribeTime  int64   `json:"subscribe_time"`
+	UnionId        string  `json:"unionid"`
+	Remark         string  `json:"remark"`
+	GroupId        int64   `json:"groupid"`
+	TagIdList      []int64 `json:"tagid_list"`
+	SubscribeScene string  `json:"subscribe_scene"`
+	QRScene        int64   `json:"qr_scene"`
+	QRSceneStr     string  `json:"qr_scene_str"`
 }
 
 type GetUserInfoRsp struct {
