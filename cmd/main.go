156f19957dc857d89bd65c554a61956ed5f6b180
smartwalle <smartwalle@gmail.com>
2021-12-16 09:40:13 +0800

    fmt

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/smartwalle/wxm"
+	"net/http"
 )
 
 func main() {
-	var officialAccount = wxm.NewOfficialAccount("wx7262a2f023e9aef8", "133850581b156b304ed23b30766aee90")
+	var client = wxm.NewOfficialAccount("wx7262a2f023e9aef8", "133850581b156b304ed23b30766aee90")
 
-	var openIdRsp, _ = officialAccount.GetUserOpenIdList("")
-	for _, openId := range openIdRsp.Data.OpenId {
-		rsp, _ := officialAccount.GetUserInfo(openId, "")
-		fmt.Println(rsp.Nickname, rsp.OpenId, rsp.UnionId)
-	}
+	var s = gin.Default()
+	s.GET("/MP_verify_VgtkH8dpacKCg867.txt", func(c *gin.Context) {
+		c.Writer.WriteString("VgtkH8dpacKCg867")
+	})
+
+	s.GET("/auth", func(c *gin.Context) {
+		c.Redirect(http.StatusTemporaryRedirect, client.GetAuthorizeURL("http://fxx.scyoule.com/", wxm.AuthScopeBase, "hehehe"))
+	})
+
+	s.GET("/", func(c *gin.Context) {
+		c.Request.ParseForm()
+		var code = c.Request.Form.Get("code")
+		var token, err = client.GetAccessToken(code)
+		if err != nil {
+			c.Writer.WriteString(err.Error())
+			return
+		}
+		c.Writer.WriteString(token.OpenId)
+	})
+
+	s.Run(":9800")
 }
