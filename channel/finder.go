d5980004b1fd31b835734db25800528d4cd1e235
SmartWalle <smartwalle@gmail.com>
2026-02-09 09:53:32 +0800

    feat: 添加获取视频号直播记录列表功能
    新增获取视频号直播记录列表的API接口及相关数据结构

--- channel/finder.go
+++ channel/finder.go
@@ -5,7 +5,9 @@ import (
 )
 
 const (
-	APIGetFinderAttr = "/channels/finderlive/get_finder_attr_by_appid"
+	APIGetFinderAttr           = "/channels/finderlive/get_finder_attr_by_appid"
+	APIGetFinderLiveRecordList = "/channels/ec/finderlive/getfinderliverecordlist"
+	APIGetFinderLiveDataList   = "/channels/ec/finderlive/get_finder_live_data_list"
 )
 
 // GetFinderAttr 获取视频号账号信息
@@ -14,6 +16,17 @@ const (
 func (c *Channel) GetFinderAttr(ctx context.Context, accessToken string) (response *GetFinderAttrResponse, err error) {
 	var request = struct{}{}
 	if err = c.Post(ctx, APIGetFinderAttr, accessToken, request, nil, &response); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+// GetFinderLiveRecordList 获取视频号直播记录列表
+//
+//	接口文档：https://developers.weixin.qq.com/doc/channels/api/channels/live/api_getfinderliverecordlist.html
+func (c *Channel) GetFinderLiveRecordList(ctx context.Context, accessToken string) (response *GetFinderLiveRecordListResponse, err error) {
+	var request = struct{}{}
+	if err = c.Post(ctx, APIGetFinderLiveRecordList, accessToken, request, nil, &response); err != nil {
 		return nil, err
 	}
 	return response, nil
--- channel/finder_type.go
+++ channel/finder_type.go
@@ -14,3 +14,20 @@ type FinderAttr struct {
 	Nickname  string `json:"nickname"`
 	FansCount int    `json:"fans_count"`
 }
+
+// GetFinderLiveRecordListResponse 获取视频号直播记录列表响应
+type GetFinderLiveRecordListResponse struct {
+	wxm.Error
+	LiveRecordList []*LiveRecord `json:"live_record_list"`
+	TotalCount     int           `json:"total_count"`
+}
+
+// LiveRecord 直播记录
+type LiveRecord struct {
+	ExportId   string `json:"export_id"`   // 直播 ID
+	CreateTime int64  `json:"create_time"` // 创建时间
+	ExpireTime int64  `json:"expire_time"` // 过期时间
+	LiveStatus int    `json:"live_status"` // 直播状态：0-未开始，1-直播中，2-已结束，3-已过期，4-已取消
+	Name       string `json:"name"`        // 直播标题
+	CoverImg   string `json:"cover_img"`   // 直播封面
+}
